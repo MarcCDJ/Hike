90021b0f4577f0bde97a24920747aef4107ce5ad
MarcCDJ <marccruzdj@gmail.com>
2023-08-23 13:33:06 -0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Marc Cruz on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
