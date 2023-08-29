705b4bd73ab1a20062c1db44ac85fdb82502248e
MarcCDJ <marccruzdj@gmail.com>
2023-08-29 11:20:42 -0400

    Add card view custom button

---
+++ Hike/Component/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  Hike
+//
+//  Created by Marc Cruz on 8/25/23.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
