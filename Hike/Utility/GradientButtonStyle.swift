e97d56b8755c1648ecf8c829cfc7b1da87c6a52e
MarcCDJ <marccruzdj@gmail.com>
2023-08-29 13:24:46 -0400

    Add style to button and animation to image.

---
+++ Hike/Utility/GradientButtonStyle.swift
@@ -0,0 +1,44 @@
+//
+//  GradientButtonStyle.swift
+//  Hike
+//
+//  Created by Marc Cruz on 8/29/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                configuration.isPressed ?
+                
+                // A: when the button is pressed
+                LinearGradient(
+                    colors: [
+                        .customGrayMedium,
+                        .customGrayLight
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                
+                :
+                
+                // B: when the button is not pressed
+                LinearGradient(
+                    colors: [
+                        .customGrayLight,
+                        .customGrayMedium
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+            .cornerRadius(40)
+    }
+}
